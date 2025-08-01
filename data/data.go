3c5a629a75df559d3a7795cc269934682f6aa4c6
alberto-debug <albertojunior848423803@gmail.com>
2025-08-02 00:06:57 +0300

    changes

--- data/data.go
+++ data/data.go
@@ -4,4 +4,5 @@ import "book-api/models"
 
 var books = []models.Book{
 	{ID: "1", NAME: "CyberSecurity Fundamentals", AUTHOR: "Alberto Junior", YEAR: 2025},
+	{ID: "2", NAME: "Programming Language", AUTHOR: "Alberto Manuel", YEAR: 2020},
 }
