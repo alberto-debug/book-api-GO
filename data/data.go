8aea27fcb6ffbf257d0ceb741129641666a1d28f
alberto-debug <albertojunior848423803@gmail.com>
2025-08-01 19:51:14 +0300

    changes

--- data/data.go
+++ data/data.go
@@ -1,5 +1,10 @@
 package data
 
-func main() {
+import "book-api/models"
 
+var books = []models.Book{
+
+	{ID: "1", Title: "The GO programming Language", Author: "Alberto Junior", Year: 2025},
+
+	{ID: "2", Title: "CyberSecurity", Author: "Alberto Junior", Year: 2020},
 }
