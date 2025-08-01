b35db04fdc7f322e2fdad7983bba781edfa9893b
alberto-debug <albertojunior848423803@gmail.com>
2025-08-01 23:57:17 +0300

    changes

--- models/book.go
+++ models/book.go
@@ -1,5 +1,8 @@
 package models
 
 type Books struct {
-	ID string `json:"id"`
+	ID     string `json:"id"`
+	NAME   string `json:"name"`
+	AUTHOR string `json:"author"`
+	YEAR   string `json:"year"`
 }
