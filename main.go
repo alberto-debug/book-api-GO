b8b4ed717c088f377f3f6188dc4d884e1fe439fd
alberto-debug <albertojunior848423803@gmail.com>
2025-08-01 20:29:50 +0300

    changes

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"book-api/handlers"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	r := mux.NewRouter()
+
+	r.HandleFunc("/books", handlers.GetBooks).Methods("GET")
 }
