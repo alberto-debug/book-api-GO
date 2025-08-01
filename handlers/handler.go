f43c9fc649eb785359379e42b53fbd63f58cfd28
alberto-debug <albertojunior848423803@gmail.com>
2025-08-02 00:28:21 +0300

    changes

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,15 +1,13 @@
 package handlers
 
 import (
-	"book-api/data"
-	"encoding/json"
 	"net/http"
 )
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	books := data.GetAllBooks()
-	json.NewEncoder(w).Encode(books)
+	books := data.
+		json.NewEncoder(w).Encode(books)
 
 }
