df69854e8d137ba2f25979b641b76d3f7ce9488e
alberto-debug <albertojunior848423803@gmail.com>
2025-08-01 20:32:25 +0300

    changes

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"book-api/handlers"
+	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -11,4 +14,8 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/books", handlers.GetBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", handlers.GetBook).Methods("GET")
+
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
