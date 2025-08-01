37e1dd1b7a4fbe9bbb24b05a0955e50f7e9f5df3
alberto-debug <albertojunior848423803@gmail.com>
2025-08-02 00:14:27 +0300

    changes

--- data/data.go
+++ data/data.go
@@ -6,3 +6,19 @@ var books = []models.Book{
 	{ID: "1", NAME: "CyberSecurity Fundamentals", AUTHOR: "Alberto Junior", YEAR: 2025},
 	{ID: "2", NAME: "Programming Language", AUTHOR: "Alberto Manuel", YEAR: 2020},
 }
+
+func GetAllBooks() []models.Book {
+	return books
+}
+
+func GetBookByID(id string) ([]models.Book, error){
+	for _, book := range books{
+		if book.ID == id {
+
+			return book`i` f
+			
+		}
+		
+	}
+
+}
