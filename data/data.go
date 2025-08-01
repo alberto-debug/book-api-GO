498bbbd43a21aaf366af2b3d715ecdead589232d
alberto-debug <albertojunior848423803@gmail.com>
2025-08-02 00:17:42 +0300

    changes

--- data/data.go
+++ data/data.go
@@ -1,6 +1,9 @@
 package data
 
-import "book-api/models"
+import (
+	"book-api/models"
+	"errors"
+)
 
 var books = []models.Book{
 	{ID: "1", NAME: "CyberSecurity Fundamentals", AUTHOR: "Alberto Junior", YEAR: 2025},
@@ -11,14 +14,16 @@ func GetAllBooks() []models.Book {
 	return books
 }
 
-func GetBookByID(id string) ([]models.Book, error){
-	for _, book := range books{
+func GetBookByID(id string) (models.Book, error) {
+	for _, book := range books {
 		if book.ID == id {
 
-			return book`i` f
-			
+			return book, nil
+
 		}
-		
+
 	}
+
+	return models.Book{}, errors.New("Book not found with id: " + id)
 
 }
